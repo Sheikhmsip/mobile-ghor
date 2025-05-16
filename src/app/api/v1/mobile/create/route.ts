745942c1d56f26aaef6236225da9ea59faae0cec
Sheikhmsip <sheikhmsip@gmail.com>
2025-05-16 17:44:14 +0600

    create prisma schema and create post method

---
+++ src/app/api/v1/mobile/create/route.ts
@@ -0,0 +1,30 @@
+import { PrismaClient } from "@/generated/prisma";
+import status from "http-status";
+import { NextRequest, NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const mobile = await prisma.mobile.create({
+    data: {
+      modelName: body.modelName,
+      brand: body.brand,
+      rom: body.rom,
+      ram: body.ram,
+      battery: body.battery,
+      display: body.display,
+      description: body.description
+,
+    },
+  });
+  return NextResponse.json(
+    {
+      message: "Mobile added successfully!",
+      data: mobile,
+    },
+    {
+      status: status.CREATED,
+    }
+  );
+}
