4762f57679382987ddbfb654325b99438ba7f129
Sheikhmsip <sheikhmsip@gmail.com>
2025-05-19 11:43:02 +0600

    added pagination

--- src/app/api/v1/mobile/get-all/route.ts
+++ src/app/api/v1/mobile/get-all/route.ts
@@ -1,18 +1,32 @@
 import { PrismaClient } from "@/generated/prisma";
 import status from "http-status";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
-export async function GET() {
-  const mobiles = await prisma.mobile.findMany();
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  // pagination
+  const page = searchParams.get("page") ? Number(searchParams.get("page")) : 1;
+  const limit = searchParams.get("limit")
+    ? Number(searchParams.get("limit"))
+    : 10;
+  const skip = limit * (page - 1);
+
+  const mobiles = await prisma.mobile.findMany({
+    take: limit,
+    skip: skip,
+  });
 
   return NextResponse.json(
     {
-    message: "Mobile data get successfully",
-    data: mobiles,
-  },
-  {
-    status: status.OK
-  }
-);
+      message: "Mobile data get successfully",
+      page: page,
+      limit: limit,
+      data: mobiles,
+    },
+    {
+      status: status.OK,
+    }
+  );
 }
